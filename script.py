763d1bdd3b3720606cc0a2f7d33739952a048381
TecInfoIAML <144251350+TecInfoIAML@users.noreply.github.com>
2023-10-09 19:15:59 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,54 @@
-import joblib  # Para modelos scikit-learn
-# OU
-import tensorflow as tf  # Para modelos TensorFlow/Keras
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Receber dados do formulário e montar em um array
+    $dados = [
+        "estado_civil" => $_POST["estado_civil"],
+        "ordem_inscricao" => $_POST["ordem_inscricao"],
+        "Curso" => $_POST["Curso"],
+        "horario_aula" => $_POST["horario_aula"],
+        "qualificacao_previa" => $_POST["qualificacao_previa"],
+        "qualificacao_anterior"=> $_POST["qualificacao_anterior"],
+        "nacionalidade" => $_POST["nacionalidade"],
+        "escolaridade_materna"=>$_POST["escolaridade_materna"],
+        "escolaridade_paterna"=>$_POST["escolaridade_paterna"],
+        "profissao_materna"=>$_POST["profissao_materna"],
+        "profissao_paterna"=>$_POST["profissao_paterna"],
+        "nota_admissao" =>$_POST["nota_admissao"],
+        "necessidades_educacionais"=>$_POST["necessidades_educacionais"],
+        "devedor"=>$_POST["devedor"],
+        "mensalidade_em_dia"=>$_POST["mensalidade_em_dia"],
+        "sexo"=>$_POST["sexo"],
+        "bolsista"=>$_POST["bolsista"],
+        "idade_na_inscricao"=>$_POST["idade_na_inscricao"],
+        "internaconal"=>$_POST["internaconal"],
+        "disciplinas_inscrita1"=>$_POST["disciplinas_inscrita1"],
+        "disciplinas_aprovadas"=>$_POST["disciplinas_aprovadas"],
+        "disciplinas_inscrita2"=>$_POST["disciplinas_inscrita2"],
+        "disciplinas_aprovadas2"=>$_POST["disciplinas_aprovadas2"]
+        
+    ];
 
-# Carregar o modelo treinado
-model = joblib.load("seu_modelo.pkl")  # Para modelos scikit-learn
-# OU
-model = tf.keras.models.load_model("seu_modelo.h5")  # Para modelos TensorFlow/Keras
+    // Enviar dados para a API Python
+    $url = 'http://21339de0-85d7-4708-a0f6-404d0b59524b.southcentralus.azurecontainer.io/prever_evasao';  // Use a URL correta da sua API Python
+    $options = [
+        'http' => [
+            'header' => 'Content-Type: application/json',
+            'method' => 'POST',
+            'content' => json_encode($dados)
+        ]
+    ];
+    $context = stream_context_create($options);
+    $resultado_api = file_get_contents($url, false, $context);
 
-# Função para fazer previsões
-def fazer_previsao(dados):
-    # Realizar pré-processamento dos dados, se necessário
-    # Fazer previsões com o modelo
-    resultado = model.predict(dados)
-    return resultado
-    
-from flask import Flask, request, jsonify
+    // Analisar a resposta JSON
+    $resultado = json_decode($resultado_api, true);
 
-app = Flask(__name__)
-
-@app.route('/prever_evasao', methods=['POST'])
-def prever_evasao():
-    dados = request.json  # Recebe os dados do formulário como JSON
-    resultado = fazer_previsao(dados)
-    return jsonify({"previsao": resultado.tolist()})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    // Verificar a previsão
+    if ($resultado["previsao"] == 1) {
+        $status_evasao = "Evasão";
+    } else {
+        $status_evasao = "Não evadido";
+    }
+    // Agora, você pode exibir $status_evasao ou realizar outras ações com base na previsão.
+}
+?>
